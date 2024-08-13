3c3e528f9a6f7211ee283f1fad0ffbfd43a1d8a4
nuriacami <73501748+nuriacami@users.noreply.github.com>
2024-08-13 15:42:23 +0200

    Create values.py

---
+++ input/values.py
@@ -0,0 +1,10 @@
+VALUES = [
+    {
+        "demarcacion": "AP7 BARCELONA SECTOR 4",
+        "etd": ['AP7-125+650-100270000000', 'AP7-129+500-100280000000', 'AP7-134+050-100290000000']
+    },
+    {
+        "demarcacion": "AP7 BARCELONA SECTOR 5",
+        "etd": ['AP7-173+000-100430000000']
+    }
+]
